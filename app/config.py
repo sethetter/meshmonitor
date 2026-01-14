1b03dc119227f4ff117eea9621f38571eba2273a
Kenton <himself@kentonhansen.com>
2026-01-13 20:14:11 -0600

    partial data migration to show everything colected by MeshLink k10n

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ MQTT_TOPIC = os.getenv('MQTT_TOPIC', 'msh/#')  # Subscribe to all Meshtastic top
 
 # Flask Configuration
 FLASK_HOST = os.getenv('FLASK_HOST', '0.0.0.0')
-FLASK_PORT = int(os.getenv('FLASK_PORT', 5000))
+FLASK_PORT = int(os.getenv('FLASK_PORT', 5001))
 FLASK_DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
 
 # Database Configuration
